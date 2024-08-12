7e0252910e45c58b2aa8a219d2165a9b4c20293b
Saltro <2373117747@qq.com>
2024-08-12 16:35:31 +0800

    feat: 支持commitlint

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/eslint/index.ts', 'src/prettier/index.ts'],
+  entry: ['src/eslint/index.ts', 'src/prettier/index.ts', 'src/commitlint/index.ts'],
 });
